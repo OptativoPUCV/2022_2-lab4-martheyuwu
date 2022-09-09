6c5ecff057f5ecb6679d21550c9d83234c1d67c3
Replit user <>
2022-09-09 02:52:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,7 +52,7 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-    HashMap **hash=(HashMap*) calloc (1,sizeof(HashMap));
+    HashMap **hash=(HashMap**) calloc (1,sizeof(HashMap));
     hash->current=-1;
     return *hash;
 }
