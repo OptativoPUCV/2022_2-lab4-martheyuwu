1dc8fbf1f67ba20b8ffd0d820d2c9cd2a48b4b85
Replit user <>
2022-09-09 12:49:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -46,7 +46,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     array=map->buckets;
     long pos=hash(key,cap);
     if (array[pos]!=NULL){
-      for (int pos;pos<cap;pos++){
+      for (pos;pos<cap;pos++){
         if (array[pos]==NULL)
           array[pos]=value;
       }
