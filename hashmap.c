026ae0ab1f9b92b614508a85a378f367166cb5e0
Replit user <>
2022-09-11 22:22:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -117,7 +117,7 @@ Pair * firstMap(HashMap * map) {
     while (1){
       if (map->buckets[pos]!=NULL){
         map->current=pos;
-        return map->buckets;
+        return map;
       }
       pos++;
     }
